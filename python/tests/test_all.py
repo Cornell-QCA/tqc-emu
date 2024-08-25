5cc72e32395990a5598ae04c1cc60554b9c2dac2
Haadi Khan <haadikhan05@gmail.com>
2024-08-25 18:42:03 -0400

    Init Commit

---
+++ python/tests/test_all.py
@@ -0,0 +1,5 @@
+import tqc_emu
+
+
+def test_sum_as_string():
+    assert tqc_emu.sum_as_string(1, 1) == '2'
---
+++ python/tqc_emu/__init__.py
@@ -0,0 +1,5 @@
+from .tqc_emu import *
+
+__doc__ = tqc_emu.__doc__
+if hasattr(tqc_emu, '__all__'):
+    __all__ = tqc_emu.__all__
