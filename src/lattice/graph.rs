f6ea06c5db5d01c4fa95283fbe2db41324998ce2
kevinsullivan0 <kevinsulli@icloud.com>
2025-02-18 17:05:35 -0500

    add basic structure/ideas to graph.rs and add String to lattice.rs

--- src/lattice/graph.rs
+++ src/lattice/graph.rs
@@ -1,4 +1,37 @@
 // For developing decoders on more general topologies than tori by using graphs to determine
 // neighbors in a more flexible manner than using 2d vectors.
 
+// A trait that all cellulations/graphs will implement
+trait Cellulation {
+    // A cellulation is pretty much the general structure where the qubits are edges and the
+    // processors/ancilli are nodes (I think).
+    // Eg for the toric code, the plaquettes look like cells.
+    // TODO: I think it would be good for this code to be as modular/extensible as is reasonable
+    // TODO: perhaps use a hashmap for cell/qubit/other element id's
+}
 
+trait Decoder {
+    // A decoder is effectively a set of rules that transform the system to mitigate error (eg
+    // harrington's rules)
+    // Other types of decoders may include floquet codes, color codes, MWPM (minimum weight perfect
+    // matching) (MWPM already has a crate I think), or even neural nets (see google's alphaqubit
+    // decoder for surface codes)
+}
+
+struct ToricCodeWithDefects {
+    // TODO: do we want hashmaps or some dedicated graph?
+    // Perhaps we could still use a lattice for this one, though it would be more difficult for
+    // later topologies (eg the genus 5 one)
+}
+
+struct HarringtonDecoder {
+
+}
+
+impl Decoder for HarringtonDecoder {
+
+}
+
+impl Cellulation for ToricCodeWithDefects {
+
+}
